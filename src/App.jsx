34bcbbc7e949f0aa5580ca0bb9439b7183693bbb
Aroon Salotra <aroonsalotra@gmail.com>
2023-01-18 19:45:13 +0000

    install react router

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,25 @@ import './App.css'
 import EmployeeCard from './components/EmployeeCard'
 import EmployeeData from "./data/EmployeeData.json"
 import Employees from './components/Employees'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
 
   return (
+    <>
 
-    <div>
-      <Employees />
-    </div>
+      <BrowserRouter>
+
+        <Routes>
+
+          <Route path='/' />
+          <Route path='employees' element={<Employees />} />
+
+
+        </Routes>
+
+      </BrowserRouter>
+    </>
 
   )
 }
