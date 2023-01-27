bcee4d4825b3aa0e1f9570634b982f51add80afe
Aroon Salotra <aroonsalotra@gmail.com>
2023-01-27 16:56:26 +0000

    add calender with holiday request function to holiday page

--- src/components/Holidays.jsx
+++ src/components/Holidays.jsx
@@ -1,12 +1,17 @@
 import { useEffect } from "react";
 import { useState } from "react";
-import { CircularProgressbar } from "react-circular-progressbar";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 import 'react-circular-progressbar/dist/styles.css';
+import { Calendar } from "react-calendar";
+// import 'react-calendar/dist/Calendar.css'
+import "../styles/Calendar.css"
 
 const Holidays = () => {
 
     const [holiday, setHoliday] = useState(null)
     const [remainder, setRemainder] = useState(null)
+    const [date, setDate] = useState(new Date());
+    const [enableSelectDate, setEnableSelectDate] = useState(false)
 
     useEffect(() => {
         const randomNumber = Math.floor(Math.random() * (80 - 20) + 20)
@@ -17,14 +22,55 @@ const Holidays = () => {
     return (
         <div className="flex flex-col items-center justify-center">
             {/* <div> */}
-            <h2>Remaining Holiday
+            <h2 className="title">Remaining Holiday
                 {/* <span>{holiday}</span> */}
             </h2>
             {/* </div> */}
 
-            <div className="w-40 h-40">
-                <CircularProgressbar value={holiday + 20} text={holiday} />
+            <div className="w-60 pt-8">
+
+                <CircularProgressbar
+                    value={holiday + 20}
+                    text={`${holiday} Hours`}
+                    styles={buildStyles({
+
+                        textSize: "0.8rem",
+                        // pathColor: "white",
+                        // trailColor: "black",
+                        backgroundColor: "black"
+
+                    })}
+                />
+
             </div>
+
+            <button onClick={() => setEnableSelectDate(!enableSelectDate)}>
+                Request Holiday
+            </button>
+
+            <Calendar
+                onChange={setDate}
+                value={date}
+                selectRange={enableSelectDate}
+            />
+
+            {date.length > 0 ? (
+                <p className='text-center'>
+                    <span className='bold'>Start:</span>{' '}
+                    {date[0].toDateString()}
+                    &nbsp;|&nbsp;
+                    <span className='bold'>End:</span> {date[1].toDateString()}
+                </p>
+            ) : (
+                <p className='text-center'>
+                    <span className='bold'>Default selected date:</span>{' '}
+                    {date.toDateString()}
+                </p>
+            )}
+
+            <small>
+                To make a holiday request, first select the desired start date, then click the "Request Holiday" button to select the end date
+            </small>
 
         </div>
     );
